29b2f47f20cbcc293b7ea33e8dc274272a2d7253
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-11-11 15:50:10 -0500

    Building Contact page. Fixed images for team.

--- client/app/contact/controllers/contact.controller.js
+++ client/app/contact/controllers/contact.controller.js
@@ -1,4 +1,13 @@
 angular.module('RCSapp.contact')
     .controller('ContactCtrl', function($scope){
 
+    $scope.firstName = null;
+    $scope.lastName = null;
+    $scope.email = null;
+    $scope.comment = null;
+
+    $scope.submitContactForm = function(){
+
+    }
+
 });
--- client/app/team/services/team.service.js
+++ client/app/team/services/team.service.js
@@ -8,32 +8,32 @@ angular.module('RCSapp.team')
                     name:'Brian Buckley',
                     position:'Director',
                     bio:'',
-                    picture:'../../images/team/staff/Ap_Creative_Stock_Header.jpg',
+                    picture:'../../images/team/staff/Brian.jpg',
                     category:'staff'
                 },
                 {
                     name:'Brandon Cooke',
                     position:'Producer',
-                    bio:'Raised in Woodstock Georgia, Brandon received his Bachelors degree in Computer Science ' +
+                    bio:'Raised in Woodstock, Georgia, Brandon received his Bachelors degree in Computer Science ' +
                         'from Kennesaw State University in 2014, and has since continued on to study '+
                         'Computer Science at Georgia Tech for his Master\'s Degree. After running Clash King Studios '+
                         'for the Georgia Melee community for over half a year, he brings his experience and tech '+
                         'savy talent to the Recursion team as our main Producer. ',
-                    picture:'../../images/team/staff/Ap_Creative_Stock_Header.jpg',
+                    picture:'../../images/team/staff/Brandon.jpg',
                     category:'staff'
                 },
                 {
-                    name:'SleepyK',
+                    name:'John \'SleepyK\' Lee',
                     position:'Outreach',
                     bio:'',
-                    picture:'../../images/team/staff/Ap_Creative_Stock_Header.jpg',
+                    picture:'../../images/team/staff/SleepyK.jpg',
                     category:'staff'
                 },
                 {
                     name:'Conor Hagan',
                     position:'Graphics Artist',
                     bio:'',
-                    picture:'../../images/team/staff/Ap_Creative_Stock_Header.jpg',
+                    picture:'../../images/team/staff/Conor.png',
                     category:'staff'
                 }
             ]
@@ -45,7 +45,7 @@ angular.module('RCSapp.team')
                     name:'Kevin \'KPAN\' Pan',
                     position:'Player',
                     bio:'',
-                    picture:'../../images/team/players/2016_bigstock_picks.jpg',
+                    picture:'../../images/team/players/Kevin.png',
                     category:'player'
                 }
             ]
