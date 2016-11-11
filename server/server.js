099faab6891f0c9febbcdcd7ca7325ef434df782
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-11-11 08:24:26 -0500

    More setup and bug fixes

--- server/server.js
+++ server/server.js
@@ -4,4 +4,6 @@ var app     = express();
 var ROOT_DIR = __dirname + '/..';
 
 app.use(express.static(ROOT_DIR + '/client'));
+
+console.log('App listening on port 8088');
 app.listen(process.env.PORT || 8088);
