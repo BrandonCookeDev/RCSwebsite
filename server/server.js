b52a56ca5a77c4f8b0bb256a9a96c08bf57c1871
Brandon Cooke <BrandonCookeDev@Gmail.com>
2016-11-13 13:54:01 +0000

    Addedapi call for getting team from the database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app     = express();
 
+var Team    = require('./models/team/team.model.js');
+
 var ROOT_DIR = __dirname + '/..';
 
 app.use(express.static(ROOT_DIR + '/client'));
+
+app.get('/api/team/getTeam/:category', function(req, res){
+	var cat = req.params.category;
+	var team = Team.find({ "category" : cat });
+	return team;
+})
+
 
 console.log('App listening on port 8088');
 app.listen(process.env.PORT || 8088);
